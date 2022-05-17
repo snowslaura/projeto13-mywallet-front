32f6b0e69531c46a1a0f06825cbff26bb3dfc524
Vinicius <vinigugelmin@hotmail.com>
2022-05-17 12:45:20 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react-dom";
-import App from "./components/App";
-import "./assets/css/style.css"
-import "./assets/css/reset.css"
-
-React.render(<App />, document.querySelector(".root"))
